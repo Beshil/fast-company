8d963680e14a4fbc063b9dd78d54b97334019a76
Beshil <ersanbievbesh@gmail.com>
2021-12-10 10:38:13 +0300

    lesson 4 pagination task 1

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    curly: ['error', 'multi-or-nest'],
+    'space-before-function-paren': ['error', 'never'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'never']
+  }
+}
